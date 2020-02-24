42a718a43cfc8a71a42295dafdaea76ceb465cce
Oliver Goroll <goroll.oliver@gmail.com>
2020-02-24 20:00:54 +0100

    Implement binary operator+ for vector

--- src/main/vector.h
+++ src/main/vector.h
@@ -6,5 +6,10 @@ class vector : public euclidean
 {
   public:
     vector(float x, float y, float z);
+
+    friend vector operator+(vector lhs, const vector& rhs)
+    {
+      return vector{lhs.x() + rhs.x(), lhs.y() + rhs.y(), lhs.z() + rhs.z()};
+    }
 };
 
--- src/test/vector_test.h
+++ src/test/vector_test.h
@@ -6,6 +6,20 @@ TEST (vector_test, vector_is_vector_tuple)
 {
   vector v{4, -4, 3};
 
-  EXPECT_TRUE(is_vector(p));
-  EXPECT_FALSE(is_point(p));
+  EXPECT_TRUE(is_vector(v));
+  EXPECT_FALSE(is_point(v));
+}
+
+TEST (vector_test, add_vectors)
+{
+  vector v1{ 3, -2, 5};
+  vector v2{-2,  3, 1};
+
+  vector v3 = v1 + v2;
+
+  EXPECT_EQ(1, v3.x());
+  EXPECT_EQ(1, v3.y());
+  EXPECT_EQ(6, v3.z());
+
+  EXPECT_TRUE(is_vector(v3));
 }
