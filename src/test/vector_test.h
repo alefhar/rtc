ea278227f34dd9ae81c05b1ecabd4682bfdb8a13
Oliver Goroll <goroll.oliver@gmail.com>
2020-02-25 20:11:07 +0100

    Add test case for vector::operator+=

--- src/test/vector_test.h
+++ src/test/vector_test.h
@@ -10,6 +10,24 @@ TEST (vector_test, vector_is_vector_tuple)
   EXPECT_FALSE(is_point(v));
 }
 
+TEST (vector_test, add_assign)
+{
+  vector v1{ 3, -2, 5};
+  vector v2{-2,  3, 1};
+
+  v1 += v2;
+
+  EXPECT_EQ(1, v1.x());
+  EXPECT_EQ(1, v1.y());
+  EXPECT_EQ(6, v1.z());
+
+  EXPECT_TRUE(is_vector(v1));
+
+  EXPECT_EQ(-2, v2.x());
+  EXPECT_EQ( 3, v2.y());
+  EXPECT_EQ( 1, v2.z());
+}
+
 TEST (vector_test, add_vectors)
 {
   vector v1{ 3, -2, 5};
@@ -22,4 +40,12 @@ TEST (vector_test, add_vectors)
   EXPECT_EQ(6, v3.z());
 
   EXPECT_TRUE(is_vector(v3));
+
+  EXPECT_EQ( 3, v1.x());
+  EXPECT_EQ(-2, v1.y());
+  EXPECT_EQ( 5, v1.z());
+
+  EXPECT_EQ(-2, v2.x());
+  EXPECT_EQ( 3, v2.y());
+  EXPECT_EQ( 1, v2.z());
 }
