80172a4712b7dea502ee606feb7cff86e176f8f1
Oliver Goroll <goroll.oliver@gmail.com>
2020-02-29 18:33:09 +0100

    Simplify asserts in vector tests

--- src/test/vector_test.h
+++ src/test/vector_test.h
@@ -17,15 +17,10 @@ TEST (vector_test, add_assign)
 
   v1 += v2;
 
-  EXPECT_EQ(1, v1.x());
-  EXPECT_EQ(1, v1.y());
-  EXPECT_EQ(6, v1.z());
-
+  EXPECT_EQ(vector(1, 1, 6), v1);
   EXPECT_TRUE(is_vector(v1));
 
-  EXPECT_EQ(-2, v2.x());
-  EXPECT_EQ( 3, v2.y());
-  EXPECT_EQ( 1, v2.z());
+  EXPECT_EQ(vector(-2, 3, 1), v2);
 }
 
 TEST (vector_test, add_vectors)
@@ -35,19 +30,11 @@ TEST (vector_test, add_vectors)
 
   vector v3 = v1 + v2;
 
-  EXPECT_EQ(1, v3.x());
-  EXPECT_EQ(1, v3.y());
-  EXPECT_EQ(6, v3.z());
-
+  EXPECT_EQ(vector(1, 1, 6), v3);
   EXPECT_TRUE(is_vector(v3));
 
-  EXPECT_EQ( 3, v1.x());
-  EXPECT_EQ(-2, v1.y());
-  EXPECT_EQ( 5, v1.z());
-
-  EXPECT_EQ(-2, v2.x());
-  EXPECT_EQ( 3, v2.y());
-  EXPECT_EQ( 1, v2.z());
+  EXPECT_EQ(vector( 3, -2, 5), v1);
+  EXPECT_EQ(vector(-2,  3, 1), v2);
 }
 
 TEST (vector_test, subtract_assign)
