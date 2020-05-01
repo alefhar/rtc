31c8def5a08b75e7c30b4eb634286c5990c0aeca
Oliver Goroll <goroll.oliver@gmail.com>
2020-05-01 15:18:16 +0200

    Implement toy cannon

--- src/main/cannon.cc
+++ src/main/cannon.cc
@@ -1,18 +1,53 @@
 #include "euclidean.h"
 
+#include <iostream>
+#include <cstdlib>
+
 struct projectile
 {
-  point _position;
-  vector _velocity;
+  point position;
+  vector velocity;
 };
 
 struct environment
 {
-  vector _gravity;
-  vector _wind;
+  vector gravity;
+  vector wind;
 };
 
+projectile tick(const projectile& proj, const environment& env)
+{
+  point position = proj.position + proj.velocity;
+  vector velocity = proj.velocity + env.gravity + env.wind;
+  return projectile{position, velocity};
+}
+
+float component(int argi, char** args) {
+  return static_cast<float>(std::atof(args[argi]));
+}
+
 int main(int argc, char** args) {
+  if (argc < 13) {
+    std::cout << "Starting position, velocity, gravity and wind parameters expected!" << std::endl;
+    return 1;
+  }
+
+  point position = point{component(1, args), component(2, args), component(3, args)};
+  vector velocity = vector{component(4, args), component(5, args), component(6, args)};
+  projectile p = projectile{position, velocity};
+
+  vector gravity = vector{component(7, args), component(8, args), component(9, args)};
+  vector wind = vector{component(10, args), component(11, args), component(12, args)};
+  environment e = environment{gravity, wind};
+
+  std::cout << "Starting conditions:\n"
+    << "projectile: position = " << p.position << ", velocity = " << p.velocity << "\n"
+    << "environment: gravity = " << e.gravity << ", wind = " << e.wind << "\n" << std::endl;
+
+  while (p.position.y() > 0) {
+    p = tick(p, e);
+    std::cout << "projectile: position = " << p.position << ", velocity = " << p.velocity << std::endl;
+  }
 
   return 0;
 }
